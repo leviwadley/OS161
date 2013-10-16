578ad435ff3024f7cc3653e320266b10605302b5
leviwadley <leviwadley@gmail.com>
2013-10-16 14:26:42 -0700

    Example Commit

--- defs.mk
+++ defs.mk
@@ -13,7 +13,7 @@
 # The things that can be set here are documented in mk/os161.config.mk.
 #
 
-OSTREE=/home/wadlev14/os161/root
+OSTREE=/home/wadlev14/OS161/root
 PLATFORM=sys161
 MACHINE=mips
 COMPAT_CFLAGS=
